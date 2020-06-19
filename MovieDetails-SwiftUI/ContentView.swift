c649ca812af97d2081cc558f50b132b7d6141511
Chris Eadie <chris.eadie@outlook.com>
2020-06-19 15:09:48 +0100

    Initial Commit

---
+++ MovieDetails-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieDetails-SwiftUI
+//
+//  Created by Chris Eadie on 19/06/2020.
+//  Copyright © 2020 ChrisEadieDesigns. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
